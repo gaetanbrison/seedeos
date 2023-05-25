2603c1b24ffe49ba82f176abab8384a27659f39c
Gaëtan <gaetan.brison@gmail.com>
2023-05-25 22:57:53 +0200

    building model

---
+++ model.py
@@ -0,0 +1,162 @@
+import pandas as pd
+from pandas import to_datetime
+from pandas.plotting import register_matplotlib_converters
+import numpy as np
+from pathlib import Path
+import base64
+from datetime import date, datetime
+
+
+from PIL import Image
+import streamlit as st
+
+
+
+from htbuilder import HtmlElement, div, hr, a, p, img, styles
+from htbuilder.units import percent, px
+import seaborn as sns
+import matplotlib.pyplot as plt
+register_matplotlib_converters()
+
+
+
+sns.set(style="whitegrid")
+pd.set_option('display.max_rows', 15)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+st.set_page_config(
+    page_title="Seedeos Report", layout="wide", page_icon="./images/flask.png"
+)
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+
+
+def main():
+    def _max_width_():
+        max_width_str = f"max-width: 1000px;"
+        st.markdown(
+            f"""
+        <style>
+        .reportview-container .main .block-container{{
+            {max_width_str}
+        }}
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
+
+
+    # Hide the Streamlit header and footer
+    def hide_header_footer():
+        hide_streamlit_style = """
+                    <style>
+                    footer {visibility: hidden;}
+                    </style>
+                    """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+    # increases the width of the text and tables/figures
+    _max_width_()
+
+    # hide the footer
+    hide_header_footer()
+
+image_edhec = Image.open('images/idehos.png')
+st.image(image_edhec, width=400)
+
+st.sidebar.header("Dashboard")
+st.sidebar.markdown("---")
+
+
+st.markdown("### Congrats you made it 🎉")
+
+
+
+
+if __name__=='__main__':
+    main()
+
+st.markdown(" ")
+st.markdown("### 👨🏼‍💻 **App Contributors:** ")
+st.image(['images/gaetan.png'], width=100,caption=["Gaëtan Brison"])
+
+st.markdown(f"####  Link to Project Website [here]({'https://seedeos.com/'}) 🚀 ")
+st.markdown(f"####  Feel free to contribute to the app and give a ⭐️")
+
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;background - color: white}
+     .stApp { bottom: 80px; }
+    </style>
+    """
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        height="auto",
+        opacity=1,
+
+    )
+
+    style_hr = styles(
+        display="block",
+        margin=px(8, 8, "auto", "auto"),
+        border_style="inset",
+        border_width=px(2)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+def footer2():
+    myargs = [
+        " Made by ",
+        link("https://seedeos.com/", "Gaëtan Brison"),
+        "👨🏼‍💻"
+    ]
+    layout(*myargs)
+
+
+if __name__ == "__main__":
+    footer2()
